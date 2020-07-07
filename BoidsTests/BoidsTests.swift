078d5ac8b72536bd0ec419ac3e11f2d17c38111c
Robert Bigelow <rfbigelow@mac.com>
2020-07-06 20:08:50 -0700

    Initial Commit

---
+++ BoidsTests/BoidsTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoidsTests.swift
+//  BoidsTests
+//
+//  Created by Robert Bigelow on 7/6/20.
+//  Copyright © 2020 Robert Bigelow. All rights reserved.
+//
+
+import XCTest
+@testable import Boids
+
+class BoidsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
